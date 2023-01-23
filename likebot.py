a1fdf4fa0c424486524057fc22adfe865bd3c96d
turonov <turonov5400gmail.com>
2023-01-23 12:36:38 +0500

    send image

--- likebot.py
+++ likebot.py
@@ -1,21 +1,42 @@
 #Import libraries
 from telegram.ext import Updater,CommandHandler,CallbackContext,MessageHandler,Filters,CallbackQueryHandler
 from telegram import Update,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardMarkup,InlineKeyboardButton
-from like_db import LikeDB
+import os
+# from like_db import LikeDB
+TOKEN = os.environ["TOKEN"]
 
 #Import TOKEN from envoirment variable
-TOKEN = '5549407151:AAGVaQx5L2bvwBYnZE3a50yycdBQfPvl1fo'
+
 
 updater = Updater(TOKEN)
 #Create start command handler
 def start(update:Update, context:CallbackContext):
     """Starts with picture all likes and all dislikes"""
+    chat_id = update.message.chat.id
+
+    bot = context.bot
+    bot.sendMessage(
+        chat_id="@ssstarts",
+        text = "Xush kelibsiz",
+    )
+def sendImage(update:Update, contex:CallbackContext):
+
+    bot = contex.bot
+    image_id = update.message.photo.file_id
+
+    
+    
+    
+
+
     
 #Create updater and dispatcher
 updater = Updater(TOKEN)
 
 #add handler to updater
 updater.dispatcher.add_handler(CommandHandler('start', start))
+
+
 
 #Start the bot
 updater.start_polling()
